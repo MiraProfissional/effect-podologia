c1fdc5a114aafabe98ce34135d1ebf43cbe74a56
MiraProfissional <jvitor.gmira@gmail.com>
2025-01-07 12:20:40 -0300

    :heavy_plus_sign: feat: adding Prettier and Eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.html", "./src/**/*.{html,js}"],
+export default {
+  content: ['./*.html', './src/**/*.{html,js}'],
   theme: {
     extend: {
       colors: {
-        redOrangeLight1: "#f5e7e4",
-        redOrangeLight2: "#d19f92",
-        redOrange: "#87341a",
-        redOrangeDark: "#853420",
+        redOrangeLight1: '#f5e7e4',
+        redOrangeLight2: '#d19f92',
+        redOrange: '#87341a',
+        redOrangeDark: '#853420',
       },
     },
   },
   plugins: [],
-}
-
+};
