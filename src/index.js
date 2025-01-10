87477e497326bc6d102c29485f145cf9d0429087
MiraProfissional <jvitor.gmira@gmail.com>
2025-01-10 16:05:59 -0300

    :recycle: refactor: changing navbar to fixed, nav links construction and hover logic

--- src/index.js
+++ src/index.js
@@ -1,50 +1,74 @@
-const homeSection = document.querySelector('#home');
+const sections = document.querySelectorAll('section');
 const navLinks = document.querySelectorAll('nav a');
-const firstNavLink = navLinks[0];
+let currentLink = navLinks[0];
 
 const activateLink = (link) => {
-  link.classList.remove('text-redOrangeLight2');
-  link.classList.add('text-redOrangeDark');
-  const spansLink = link.querySelectorAll('span');
-  spansLink[1].classList.add('bg-redOrangeDark', 'w-full');
+  link.classList.add('active');
 };
 
 const deactivateLink = (link) => {
-  link.classList.remove('text-redOrangeDark');
-  link.classList.add('text-redOrangeLight2');
-  const spansLink = link.querySelectorAll('span');
-  spansLink[1].classList.remove('bg-redOrangeDark', 'w-full');
+  link.classList.remove('active');
 };
 
-const activeFirstItemNavbarHover = (entries) => {
-  if (entries[0].isIntersecting) {
-    activateLink(firstNavLink);
+const deactivateAllLinks = (currentLink) => {
+  navLinks.forEach((link) => {
+    if (link == currentLink) {
+      return;
+    }
+    deactivateLink(link);
+  });
+};
+
+const getCorrespondingNavLink = (sectionId) => {
+  switch (sectionId) {
+    case 'home':
+      return navLinks[0];
+    case 'services':
+      return navLinks[1];
+    case 'infrastructure':
+      return navLinks[2];
+    case 'reviews':
+      return navLinks[3];
+    case 'location':
+      return navLinks[4];
   }
 };
 
+const activeCurrentHoverLink = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      currentLink = getCorrespondingNavLink(entry.target.id);
+      activateLink(currentLink);
+      deactivateAllLinks(currentLink);
+    }
+  });
+};
+
 const observerOptions = {
-  threshold: 0,
+  threshold: 0.8,
 };
 
 const observer = new IntersectionObserver(
-  activeFirstItemNavbarHover,
+  activeCurrentHoverLink,
   observerOptions
 );
 
-observer.observe(homeSection);
+sections.forEach((section) => {
+  observer.observe(section);
+});
 
 navLinks.forEach((link) => {
   link.addEventListener('mouseover', () => {
-    if (link !== firstNavLink) {
-      deactivateLink(firstNavLink);
+    if (link !== currentLink) {
+      deactivateLink(currentLink);
     }
   });
 });
 
 navLinks.forEach((link) => {
   link.addEventListener('mouseout', () => {
-    if (link !== firstNavLink) {
-      activateLink(firstNavLink);
+    if (link !== currentLink) {
+      activateLink(currentLink);
     }
   });
 });
