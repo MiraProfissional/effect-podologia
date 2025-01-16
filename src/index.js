06839daa1a90456618fb1f045d2a7f9c5a312932
MiraProfissional <jvitor.gmira@gmail.com>
2025-01-16 12:07:40 -0300

    :sparkles: feat: creating reviews section

--- src/index.js
+++ src/index.js
@@ -6,3 +6,28 @@ menuToggle.addEventListener('click', () => {
   menuToggle.setAttribute('aria-expanded', !isExpanded);
   mobileNavbar.classList.toggle('hidden');
 });
+
+const reviewCards = document.querySelectorAll('.reviewCard');
+
+const activeReviewCardsAnimation = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visibleReviewCard');
+    } else {
+      entry.target.classList.remove('visibleReviewCard');
+    }
+  });
+};
+
+const reviewCardsObserverOptions = {
+  threshold: 0.8,
+};
+
+const reviewCardsObserver = new IntersectionObserver(
+  activeReviewCardsAnimation,
+  reviewCardsObserverOptions
+);
+
+reviewCards.forEach((card) => {
+  reviewCardsObserver.observe(card);
+});
