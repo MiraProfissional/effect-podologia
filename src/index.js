7ce0d366650a2dc0ae3c239747ac43383787665a
MiraProfissional <jvitor.gmira@gmail.com>
2025-01-21 20:00:23 -0300

    :construction: feat: building all page for lg (min-1024px) screens

--- src/index.js
+++ src/index.js
@@ -31,3 +31,80 @@ const reviewCardsObserver = new IntersectionObserver(
 reviewCards.forEach((card) => {
   reviewCardsObserver.observe(card);
 });
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.navLink');
+let currentLink = navLinks[0];
+
+const activateLink = (link) => {
+  link.classList.add('active');
+};
+
+const deactivateLink = (link) => {
+  link.classList.remove('active');
+};
+
+const deactivateAllLinks = (currentLink) => {
+  navLinks.forEach((link) => {
+    if (link == currentLink) {
+      return;
+    }
+    deactivateLink(link);
+  });
+};
+
+const getCorrespondingNavLink = (sectionId) => {
+  switch (sectionId) {
+    case 'home':
+      return navLinks[0];
+    case 'services':
+      return navLinks[1];
+    case 'infrastructure':
+      return navLinks[2];
+    case 'reviews':
+      return navLinks[3];
+    case 'location':
+      return navLinks[4];
+  }
+};
+
+const activeCurrentHoverLink = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log('entry', entry);
+      console.log('entry.target.id', entry.target.id);
+      currentLink = getCorrespondingNavLink(entry.target.id);
+      activateLink(currentLink);
+      deactivateAllLinks(currentLink);
+    }
+  });
+};
+
+const sectionObserverOptions = {
+  threshold: 0.5,
+};
+
+const sectionObserver = new IntersectionObserver(
+  activeCurrentHoverLink,
+  sectionObserverOptions
+);
+
+sections.forEach((section) => {
+  sectionObserver.observe(section);
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('mouseover', () => {
+    if (link !== currentLink) {
+      deactivateLink(currentLink);
+    }
+  });
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('mouseout', () => {
+    if (link !== currentLink) {
+      activateLink(currentLink);
+    }
+  });
+});
